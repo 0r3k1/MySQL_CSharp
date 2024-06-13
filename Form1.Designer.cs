acf360cd3074e36edf6669844d4762cf5374075d
Oreki <45185337+0r3k1@users.noreply.github.com>
2024-06-12 21:18:35 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace MySQL_CSharp {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lbl_txt = new System.Windows.Forms.Label();
+            this.dtg_empleados = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBox_nombre = new System.Windows.Forms.TextBox();
+            this.txtBox_puesto = new System.Windows.Forms.TextBox();
+            this.txtBox_edad = new System.Windows.Forms.TextBox();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.btn_editar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_empleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_txt
+            // 
+            this.lbl_txt.AutoSize = true;
+            this.lbl_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_txt.Location = new System.Drawing.Point(171, 564);
+            this.lbl_txt.Name = "lbl_txt";
+            this.lbl_txt.Size = new System.Drawing.Size(92, 31);
+            this.lbl_txt.TabIndex = 1;
+            this.lbl_txt.Text = "label1";
+            // 
+            // dtg_empleados
+            // 
+            this.dtg_empleados.AllowUserToAddRows = false;
+            this.dtg_empleados.AllowUserToDeleteRows = false;
+            this.dtg_empleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_empleados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dtg_empleados.Location = new System.Drawing.Point(12, 226);
+            this.dtg_empleados.Name = "dtg_empleados";
+            this.dtg_empleados.ReadOnly = true;
+            this.dtg_empleados.Size = new System.Drawing.Size(409, 335);
+            this.dtg_empleados.TabIndex = 2;
+            this.dtg_empleados.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtg_empleados_CellClick);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 40;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 120;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Puesto";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 110;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Edad";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 80;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Puesto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Edad:";
+            // 
+            // txtBox_nombre
+            // 
+            this.txtBox_nombre.Location = new System.Drawing.Point(72, 16);
+            this.txtBox_nombre.Name = "txtBox_nombre";
+            this.txtBox_nombre.Size = new System.Drawing.Size(345, 20);
+            this.txtBox_nombre.TabIndex = 6;
+            // 
+            // txtBox_puesto
+            // 
+            this.txtBox_puesto.Location = new System.Drawing.Point(72, 39);
+            this.txtBox_puesto.Name = "txtBox_puesto";
+            this.txtBox_puesto.Size = new System.Drawing.Size(345, 20);
+            this.txtBox_puesto.TabIndex = 7;
+            // 
+            // txtBox_edad
+            // 
+            this.txtBox_edad.Location = new System.Drawing.Point(72, 64);
+            this.txtBox_edad.Name = "txtBox_edad";
+            this.txtBox_edad.Size = new System.Drawing.Size(345, 20);
+            this.txtBox_edad.TabIndex = 8;
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Location = new System.Drawing.Point(20, 94);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(394, 27);
+            this.btn_agregar.TabIndex = 9;
+            this.btn_agregar.Text = "Agregar";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            this.btn_agregar.Click += new System.EventHandler(this.btn_agregar_Click);
+            // 
+            // btn_editar
+            // 
+            this.btn_editar.Location = new System.Drawing.Point(20, 127);
+            this.btn_editar.Name = "btn_editar";
+            this.btn_editar.Size = new System.Drawing.Size(394, 27);
+            this.btn_editar.TabIndex = 10;
+            this.btn_editar.Text = "Editar";
+            this.btn_editar.UseVisualStyleBackColor = true;
+            this.btn_editar.Click += new System.EventHandler(this.btn_editar_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(20, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(394, 27);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn_eliminar);
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Location = new System.Drawing.Point(20, 160);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(394, 27);
+            this.btn_buscar.TabIndex = 12;
+            this.btn_buscar.Text = "Buscar";
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 604);
+            this.Controls.Add(this.btn_buscar);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn_editar);
+            this.Controls.Add(this.btn_agregar);
+            this.Controls.Add(this.txtBox_edad);
+            this.Controls.Add(this.txtBox_puesto);
+            this.Controls.Add(this.txtBox_nombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtg_empleados);
+            this.Controls.Add(this.lbl_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_empleados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_txt;
+        private System.Windows.Forms.DataGridView dtg_empleados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBox_nombre;
+        private System.Windows.Forms.TextBox txtBox_puesto;
+        private System.Windows.Forms.TextBox txtBox_edad;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.Button btn_editar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_buscar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace MySQL_CSharp {
+    public partial class Form1 : Form {
+
+        MySqlConnection myCon;
+        string id = "NULL";
+
+        public Form1() {
+            InitializeComponent();
+
+
+            lbl_txt.Text = "Desconectado";
+            lbl_txt.Left = (this.ClientSize.Width - lbl_txt.Size.Width) / 2;
+
+            conectar();
+            llenarTabla();
+        }
+
+        private void conectar() {
+            try {
+                string server = "localhost";
+                string db = "empleados";
+                string user = "root";
+                string pwd = "123456";
+                string con = $"server={server};Port={3306};database={db};Uid={user};pwd={pwd};";
+
+                myCon = new MySqlConnection(con);
+                myCon.Open();
+
+                lbl_txt.Text = "Conectado";
+                lbl_txt.ForeColor = System.Drawing.Color.Green;
+                lbl_txt.Left = (this.ClientSize.Width - lbl_txt.Size.Width) / 2;
+            } catch (Exception err) {
+                lbl_txt.Text = $"Error de coneccion";
+                lbl_txt.ForeColor = System.Drawing.Color.Red;
+                lbl_txt.Left = (this.ClientSize.Width - lbl_txt.Size.Width) / 2;
+
+                Console.WriteLine(err);
+            }
+        }
+
+        private void llenarTabla(string query = "select * from empleados;") {
+
+            MySqlCommand comandDB = new MySqlCommand(query, myCon);
+            comandDB.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            dtg_empleados.Rows.Clear();
+            dtg_empleados.Refresh();
+
+            try {
+                reader = comandDB.ExecuteReader();
+                if (reader.HasRows) {
+                    while (reader.Read()) {
+                        int index = dtg_empleados.Rows.Add();
+                        dtg_empleados.Rows[index].Cells[0].Value = reader.GetString(0);
+                        dtg_empleados.Rows[index].Cells[1].Value = reader.GetString(1);
+                        dtg_empleados.Rows[index].Cells[2].Value = reader.GetString(2);
+                        dtg_empleados.Rows[index].Cells[3].Value = reader.GetString(3);
+                    }
+                }
+
+                reader.Close();
+                id = "NULL";
+            } catch (Exception err) {
+                MessageBox.Show($"{err}");
+            }
+            
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e) {
+            string were = "WHERE 1=1 ";
+
+            if (txtBox_nombre.Text != "") were += $"and Nombre LIKE '%{txtBox_nombre.Text}%'";
+            if (txtBox_puesto.Text != "") were += $"and Puesto LIKE '%{txtBox_puesto.Text}%'";
+            if (txtBox_edad.Text != "") were += $"and Edad LIKE '%{txtBox_edad.Text}%'";
+
+            string query = $"SELECT * FROM empleados {were};";
+
+            llenarTabla(query);
+        }
+
+        private void btn_agregar_Click(object sender, EventArgs e) {
+            string msg = "";
+
+            if (txtBox_nombre.Text == "") msg = "el nombre no puede estar vacio\n";
+            if (txtBox_puesto.Text == "") msg += "el puesto no puede estar vacio\n";
+            if (txtBox_edad.Text == "") msg += "la edad no puede estar vacia\n";
+
+            if(msg == "") {
+                string query = $"INSERT INTO empleados (Nombre, Puesto, Edad) VALUES " +
+                    $"('{txtBox_nombre.Text}', '{txtBox_puesto.Text}', {txtBox_edad.Text});";
+
+                txtBox_nombre.Text = "";
+                txtBox_puesto.Text = "";
+                txtBox_edad.Text = "";
+
+                llenarTabla(query);
+                llenarTabla();
+
+
+            } else MessageBox.Show(msg);
+        }
+
+        private void dtg_empleados_CellClick(object sender, DataGridViewCellEventArgs e) {
+            int index = e.RowIndex;
+
+            dtg_empleados.Rows[index].Selected = true;
+
+            id = dtg_empleados.Rows[index].Cells[0].Value.ToString();
+            txtBox_nombre.Text = dtg_empleados.Rows[index].Cells[1].Value.ToString();
+            txtBox_puesto.Text = dtg_empleados.Rows[index].Cells[2].Value.ToString();
+            txtBox_edad.Text = dtg_empleados.Rows[index].Cells[3].Value.ToString();
+        }
+
+        private void btn_editar_Click(object sender, EventArgs e) {
+            string query = $"UPDATE empleados SET nombre = '{txtBox_nombre.Text}', puesto = '{txtBox_puesto.Text}', edad = '{txtBox_edad.Text}' WHERE id = {id}";
+
+            llenarTabla(query);
+            llenarTabla();
+        }
+
+        private void btn_eliminar(object sender, EventArgs e) {
+            MessageBox.Show(id);
+            if(id != "NULL") {
+                string query = $"DELETE FROM empleados WHERE id = {id};";
+                llenarTabla(query);
+                llenarTabla();
+            }
+        }
+    }
+}
